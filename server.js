f8aa71d0a43e42030711e7c6ea0c9f83e3e11e0e
unknown <henryarcv2@gmail.com>
2023-10-26 07:22:45 +0800

    test

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
-const path = require('path')
-const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
+// const path = require('path')
+// const express = require('express');
+// const cors = require('cors');
+// const bodyParser = require('body-parser');
+// const cookieParser = require('cookie-parser');
 
-const playRouter = require('./src/routers/play');
-const levelRouter = require('./src/routers/level');
-const accountRouter = require('./src/routers/account');
-const refreshRouter = require('./src/routers/refresh');
+// const playRouter = require('./src/routers/play');
+// const levelRouter = require('./src/routers/level');
+// const accountRouter = require('./src/routers/account');
+// const refreshRouter = require('./src/routers/refresh');
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'dist')))
+//app.use(express.static(path.join(__dirname, 'dist')))
 // const whiteList = ['http://localhost:3000', 'http://localhost:5173'];
 // const corsOptions = {
 //     origin: (origin, callback) =>{
@@ -33,13 +33,15 @@ app.use(express.static(path.join(__dirname, 'dist')))
 // }
 // app.use(credentials);
 // app.use(cors(corsOptions));
-app.use(bodyParser.json());
-app.use(cookieParser());
-app.use('/api/refresh', refreshRouter);
-app.use('/api/account', accountRouter);
-app.use('/api/play', playRouter);
-app.use('/api/levels', levelRouter);
+// app.use(bodyParser.json());
+// app.use(cookieParser());
+// app.use('/api/refresh', refreshRouter);
+// app.use('/api/account', accountRouter);
+// app.use('/api/play', playRouter);
+// app.use('/api/levels', levelRouter);
 const port = 3000;
 app.listen(port, () => {
     console.log(`Server is up and running on port ${port}...`);
 });
+
+app.get('/', (req, res) => res.json("testtest"));
